7aa0d7398c15eb96538497bf4099425ccb18f5b8
vladimir0646 <sobolevbobolev@mail.ru>
2023-04-26 17:34:35 +0300

    feat: create digit buttons

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,33 @@
 //     console.log(event)
 // }
 //
-// document.onkeyup = function (event) {
-//     console.log("отпустил")
-//     console.log(event)
-// }
+let digits = [];
+document.onkeyup = function (event) {
+
+    // console.log(event.key)
+    digits.push(event.key)
+    console.log(digits)
+}
 
 // structure of page
 const Classes = {
     'BODY': 'body',
-    'CONTAINER': 'container'
+    'CONTAINER': 'container',
+    'KEY': 'key',
+    'SYMBOL': 'key__symbol',
+    'DIGIT': 'key__digit'
+}
+
+// digits
+const DIGIT_CODE = ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0'];
+const DIGIT_KEY = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='];
+const SYMBOL_CODE = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal'];
+const SYMBOL_KEY = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
+// remove tab event
+window.onkeydown = evt => {
+    if (evt.key == 'Tab') {
+        evt.preventDefault();
+    }
 }
 
 
@@ -24,7 +42,17 @@ function createNewElement (parent, style, tag = 'div') {
 }
 const BODY = document.querySelector('.' + Classes['BODY']);
 const CONTAINER = createNewElement(BODY, 'CONTAINER');
-
+// const DIGITS = new Array();
 console.log(CONTAINER)
 // const CONTAINER = createElement('div')
 console.log(BODY)
+
+// create DIGITS keys
+
+const DIGIT_KEYS = SYMBOL_KEY.map((el, index) => {
+    let key = createNewElement(CONTAINER, 'KEY');
+    let symbol = createNewElement(key, 'SYMBOL');
+    symbol.append(el)
+    let digit = createNewElement(key, 'DIGIT');
+    digit.append(DIGIT_KEY[index])
+})
